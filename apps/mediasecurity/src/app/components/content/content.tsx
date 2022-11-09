881fb4c1c6ed593c944d426b59f631f114a3351b
onwd <onwdit@gmail.com>
2022-11-10 03:53:45 +0600

    feat: add voice

--- apps/mediasecurity/src/app/components/content/content.tsx
+++ apps/mediasecurity/src/app/components/content/content.tsx
@@ -2,6 +2,8 @@ import styles from './content.module.scss';
 import Typist from 'react-typist-component';
 import { ReactNode, useState } from 'react';
 
+const TYPING_DELAY = 50;
+
 export function Content({ children, onTypingDone }: { children: ReactNode, onTypingDone: () => void }) {
   const [shouldShowCursor, setShouldShowCursor] = useState<boolean>(true);
 
@@ -13,7 +15,7 @@ export function Content({ children, onTypingDone }: { children: ReactNode, onTyp
   return (
     <div className={styles['content']}>
       <Typist
-        typingDelay={50}
+        typingDelay={TYPING_DELAY}
         cursor={(shouldShowCursor) ? <span className={styles['cursor']}>█</span> : ''}
         onTypingDone={handleTypingDone}
       >
